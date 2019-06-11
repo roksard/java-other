0ed9be46be3b4f1e19c75093c1f67162368661f7
roksard <roksard@gmail.com>
2019-06-11 23:29:30 +0600

    webIndexer DAO: add method for loading db connection params

--- webIndexerApp/webindexer-dao/src/main/java/rx/webindexer/dao/Keeper.java
+++ webIndexerApp/webindexer-dao/src/main/java/rx/webindexer/dao/Keeper.java
@@ -1,37 +1,64 @@
 package rx.webindexer.dao;
 
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
 
 public class Keeper {
-	public static final String LOCAL_DEFAULT_DIR = ""; //use current dir
-	public static final String STATS_DEFAULT_FILE = "stats_data.dat"; 
-	public static final String USERS_DEFAULT_FILE = "users.dat";
-	
-	public static <T extends Serializable> void saveToExternal(String fileName, T object) throws IOException {
-		ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(fileName));
-		out.writeObject(object);
-		out.close();
+	static Properties properties = null;
+	static final String PROPERTIES_FILE = "webindexer.properties";
+	static final String DB_HOST_PROPERTY = "db.host";
+	static final String DB_USER_PROPERTY = "db.user";
+	static final String DB_PASSWORD_PROPERTY = "db.pw";
+
+	public static Properties getProperties() throws FileNotFoundException, IOException {
+		// настройки properties используются в течение работы программы
+		// доступ к ним осуществляется с помощью текущего метода
+		if (properties != null)
+			return properties;
+		try (FileInputStream fis = new FileInputStream(PROPERTIES_FILE);) {
+			Properties prop = new Properties();
+			prop.load(fis);
+			return prop;
+		}
 	}
-	
-	public static <T extends Serializable> T loadFromExternal(String fileName) throws ClassNotFoundException, IOException {
-		ObjectInputStream in = new ObjectInputStream(new FileInputStream(fileName));
-		@SuppressWarnings("unchecked")
-		T result = (T)in.readObject();
-		in.close();
-		return result;
+
+	public static String getDBUser() throws FileNotFoundException, IOException {
+		return getProperties().getProperty(DB_USER_PROPERTY);
 	}
-	
-	public static <T extends Serializable> T loadAsResource(String resourceName) throws IOException, ClassNotFoundException {
-		ObjectInputStream in = new ObjectInputStream(Keeper.class.getResourceAsStream(resourceName));
-		@SuppressWarnings("unchecked")
-		T result = (T)in.readObject();
-		in.close();
-		return result;
+
+	public static String getDBHost() throws FileNotFoundException, IOException {
+		return getProperties().getProperty(DB_HOST_PROPERTY);
 	}
-	
+
+	public static String getDBPassword() throws FileNotFoundException, IOException {
+		return getProperties().getProperty(DB_PASSWORD_PROPERTY);
+	}
+
+	public static void initDataBase() throws Exception {
+		Connection con = null; 
+		Statement stmt = null; 
+		ResultSet rs = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection(getDBHost(), getDBUser(), getDBPassword());
+			stmt = con.createStatement();
+			stmt.execute("SELECT 1 FROM WI-USERS");
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			if(con != null)
+				con.close();
+			if(stmt != null) 
+				stmt.close();
+			if(rs != null)
+				rs.close();
+		}
+	}
 }
