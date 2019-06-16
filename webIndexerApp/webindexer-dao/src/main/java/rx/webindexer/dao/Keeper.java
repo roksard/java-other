69155523e191d160f1af877a48229a1615b3eb37
roksard <roksard@gmail.com>
2019-06-16 20:03:40 +0600

    webIndexerApp: static->non static Keeper, add tests
    Keeper.java: change static methods to non static, add some DB
    interaction methods
    Add tests for DB interaction methods

--- webIndexerApp/webindexer-dao/src/main/java/rx/webindexer/dao/Keeper.java
+++ webIndexerApp/webindexer-dao/src/main/java/rx/webindexer/dao/Keeper.java
@@ -5,7 +5,11 @@ import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -17,13 +21,23 @@ import java.util.Properties;
 
 public class Keeper {
 	static Properties properties = null;
-	public static final String PROPERTIES_FILE = "webindexer.properties";
-	public static final String DB_HOST_PROPERTY = "db.host";
-	public static final String DB_USER_PROPERTY = "db.user";
-	public 	static final String DB_PASSWORD_PROPERTY = "db.pw";
-	public static final String DB_TABLE = "WIUSERS";
+	String PROPERTIES_FILE = "webindexer.properties";
+	String DB_HOST_PROPERTY = "db.host";
+	String DB_USER_PROPERTY = "db.user";
+	String DB_PASSWORD_PROPERTY = "db.pw";
+	String DB_TABLE = "WIUSERS";
+	Connection connection = null;
 
-	public static Properties getProperties() throws FileNotFoundException, IOException {
+	public Keeper() {
+	}
+
+	public Keeper(boolean connectNow) throws SQLException, Exception {
+		if (connectNow) {
+			establishConnection();
+		}
+	}
+
+	public Properties getProperties() throws FileNotFoundException, IOException {
 		// настройки properties используются в течение работы программы
 		// доступ к ним осуществляется с помощью текущего метода
 		if (properties == null) {
@@ -35,82 +49,109 @@ public class Keeper {
 		return properties;
 	}
 
-	public static String getDBAccessUser() throws Exception {
+	public String getDBAccessUser() throws Exception {
 		return getProperties().getProperty(DB_USER_PROPERTY);
 	}
 
-	public static String getDBAccessHost() throws Exception {
+	public String getDBAccessHost() throws Exception {
 		return getProperties().getProperty(DB_HOST_PROPERTY);
 	}
 
-	public static String getDBAccessPassword() throws Exception {
+	public String getDBAccessPassword() throws Exception {
 		return getProperties().getProperty(DB_PASSWORD_PROPERTY);
 	}
 
-	public static Connection getConnection() throws SQLException, Exception {
+	public Connection establishConnection() throws SQLException, Exception {
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		return DriverManager.getConnection(getDBAccessHost(), getDBAccessUser(), getDBAccessPassword());
+		connection = DriverManager.getConnection(getDBAccessHost(), getDBAccessUser(), getDBAccessPassword());
+		return connection;
 	}
 
-	public static void closeConnection(Connection con) throws SQLException {
-		if (con != null)
-			con.close();
+	public Connection getConnection() {
+		return connection;
 	}
-	
+
+	public void closeConnection() throws SQLException {
+		if (connection != null)
+			connection.close();
+	}
+
 	public static byte[] objectToByteArray(Object obj) throws IOException {
-		try (
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			ObjectOutputStream oos = new ObjectOutputStream(baos);
-			) {
+		try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+				ObjectOutputStream oos = new ObjectOutputStream(baos);) {
 			oos.writeObject(obj);
 			return baos.toByteArray();
-		} 
+		}
 	}
 
-	public static void addUser(String user, String password, LinkedList<StatsUnit> stats) throws Exception {
-		Connection con = null;
+	public PreparedStatement insertUser(String user, String password, LinkedList<StatsUnit> stats) throws Exception {
 		PreparedStatement stmt = null;
-		try  {
-			con = getConnection();
-			stmt = con.prepareStatement("INSERT INTO WIUSERS (user, password, webstats) values (?,?,?)");
-			stmt.setString(1, "'"+user+"'");
-			stmt.setString(2, "'"+password+"'");
-			stmt.setBlob(3, new ByteArrayInputStream( objectToByteArray(stats) ));
+		stmt = connection.prepareStatement("INSERT INTO WIUSERS (user, password, webstats) values (?,?,?)");
+		stmt.setString(1, user);
+		stmt.setString(2, password);
+		stmt.setBlob(3, new ByteArrayInputStream(objectToByteArray(stats)));
+		stmt.execute();
+		return stmt;
+	}
+
+	public PreparedStatement updateUserStats(String user, LinkedList<StatsUnit> stats) throws Exception {
+		PreparedStatement stmt = null;
+		try {
+			stmt = connection.prepareStatement("UPDATE WIUSERS SET webstats = ? WHERE user = ?");
+			stmt.setBlob(1, new ByteArrayInputStream(objectToByteArray(stats)));
+			stmt.setString(2, user);
 			stmt.execute();
+			return stmt;
 		} finally {
-			closeConnection(con);
-			if(stmt != null) 
+			if (stmt != null)
 				stmt.close();
 		}
 
 	}
+
 	
-	public static void updateUserStats(String user, LinkedList<StatsUnit> stats) throws Exception {
-		Connection con = null;
+	public String getUserPassword(String user) throws Exception {
 		PreparedStatement stmt = null;
-		try  {
-			con = getConnection();
-			stmt = con.prepareStatement("UPDATE WIUSERS SET webstats = ? WHERE user = ?");
-			stmt.setBlob(1, new ByteArrayInputStream( objectToByteArray(stats) ));
-			stmt.setString(2, "'"+user+"'");
+		ResultSet rs = null;
+		String result = null;
+		try {
+			stmt = connection.prepareStatement("SELECT password FROM WIUSERS WHERE user = ?");
+			stmt.setString(1, user);
 			stmt.execute();
+			rs = stmt.getResultSet();
+			if (rs != null) {
+				if (rs.next())
+					result = rs.getString(1);
+			}
+			return result;
 		} finally {
-			closeConnection(con);
-			if(stmt != null) 
+			if (stmt != null)
 				stmt.close();
 		}
 
 	}
 
-	public static void main(String[] args) throws Exception {
-		String createTable = "CREATE TABLE `WIUSERS` (\r\n" + "  `id` INT NOT NULL AUTO_INCREMENT,\r\n"
-				+ "  `user` VARCHAR(64) NOT NULL,\r\n" + "  `password` VARCHAR(64) NOT NULL,\r\n"
-				+ "  `webstats` BLOB NULL,\r\n" + "  PRIMARY KEY (`id`),\r\n"
-				+ "  UNIQUE INDEX `id_UNIQUE` (`id` ASC),\r\n"
-				+ "  UNIQUE INDEX `user_UNIQUE` (`user` ASC))\r\n" + "ENGINE = InnoDB\r\n"
-				+ "DEFAULT CHARACTER SET = utf8\r\n" + "COLLATE = utf8_unicode_ci\r\n"
-				+ "COMMENT = 'WebIndexer app users and web stats table';\r\n";
-		System.out.println(createTable);
+	public LinkedList<StatsUnit> getUserStats(String user) throws Exception {
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		try {
+			stmt = connection.prepareStatement("SELECT webstats FROM WIUSERS WHERE user = ?");
+			stmt.setString(1, user);
+			stmt.execute();
+			rs = stmt.getResultSet();
+			if (rs != null) {
+				if (rs.next()) {
+					Blob blob = rs.getBlob(1);
+					ObjectInputStream ois = new ObjectInputStream(blob.getBinaryStream());
+					return (LinkedList<StatsUnit>) ois.readObject();
+				}
+			}
+			return null;
+		} finally {
+			if (stmt != null)
+				stmt.close();
+		}
+
 	}
 
 	/*
@@ -119,13 +160,11 @@ public class Keeper {
 	 * хранится LinkedList<Stats> - связанный список, в котором хранятся данные о
 	 * проиндексированных пользователем страницах
 	 */
-	public static void initDataBase() throws Exception {
-		Connection con = null;
+	public void initDataBase() throws Exception {
 		Statement stmt = null;
 		ResultSet rs = null;
 		try {
-			con = getConnection();
-			stmt = con.createStatement();
+			stmt = connection.createStatement();
 			try {
 				// запрос, чтобы проверить что таблица существует
 				stmt.execute("SELECT 1 FROM WIUSERS");
@@ -145,7 +184,6 @@ public class Keeper {
 			}
 
 		} finally {
-			closeConnection(con);
 			if (stmt != null)
 				stmt.close();
 			if (rs != null)
---
+++ webIndexerApp/webindexer-dao/src/test/java/rx/webindexer/dao/TestKeeper.java
@@ -0,0 +1,140 @@
+package rx.webindexer.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.TreeMap;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestKeeper extends Keeper {
+	String PROPERTIES_TEST_FILE = "src/test/resources/webindexer_test.properties";
+	User testUser = null;
+	LinkedList<StatsUnit> stats = null;
+
+	boolean tableExists() throws Exception {
+		Statement stmt = null;
+		try {
+			stmt = connection.createStatement();
+			try {
+				// запрос, чтобы проверить что таблица существует
+				stmt.execute("SELECT 1 FROM WIUSERS");
+			} catch (SQLException e) {
+				// 42S02: Table or view doesn't exist
+				if (e.getSQLState().equalsIgnoreCase("42S02"))
+					return false;
+			}
+		} finally {
+			if (stmt != null)
+				stmt.close();
+		}
+		return true;
+	}
+
+	void dropTable() throws Exception {
+		Statement stmt = null;
+		try {
+			stmt = connection.createStatement();
+			stmt.execute("DROP TABLE wiusers");
+		} finally {
+			if (stmt != null)
+				stmt.close();
+		}
+	}
+	
+	public void createTestUser() {
+		testUser = new User("user1", "password1");
+		StatsUnit statsUnit = new StatsUnit();
+		statsUnit.calculated = true;
+		statsUnit.letterFrequency = new LinkedList<TreeMap<Character, Float>>();
+		TreeMap<Character, Float> letterFreq = new TreeMap<Character, Float>();
+		letterFreq.put('c', 3.0f);
+		letterFreq.put('a', 0.5f);
+		TreeMap<Character, Float> letterFreq2 = new TreeMap<Character, Float>(letterFreq);
+		letterFreq2.put('x', 5.0f);
+		statsUnit.letterFrequency.add(letterFreq);
+		statsUnit.letterFrequency.add(letterFreq2);
+		statsUnit.setUniqueWordsPercent(50.0f);
+		stats = new LinkedList<>();
+		Collections.addAll(stats, statsUnit);
+	}
+	
+	@Before
+	public void beforeTest() {
+		try {
+			createTestUser();
+			PROPERTIES_FILE = PROPERTIES_TEST_FILE;
+			establishConnection();
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+	}
+	
+	@After
+	public void afterTest() {
+		if(connection != null)
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+	}
+
+	@Test //@Ignore
+	public void _a_shouldInitDB() {
+		try {
+			if (tableExists())
+				dropTable();
+			initDataBase();
+			Assert.assertTrue(tableExists());
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+	}
+	
+	@Test
+	public void _b_shouldInsertUser() {
+		try {
+			PreparedStatement stmt =
+			insertUser(testUser.getLogin(), testUser.getPassword(), stats);
+			boolean updated1 = stmt.getUpdateCount() == 1;
+			Assert.assertTrue(updated1);
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+	}
+	
+	@Test
+	public void _c_shouldGetUserStats() {
+		try {
+			LinkedList<StatsUnit> stats1 = getUserStats(testUser.getLogin());
+			Assert.assertTrue(stats1.equals(stats));
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+	}
+	
+	@Test
+	public void _d_shouldGetUserPassword() {
+		try {
+			String pass = getUserPassword(testUser.getLogin());
+			Assert.assertTrue(pass.equals(testUser.getPassword()));
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+	}
+}
