df54dd52f9ff6a6d208d1d30066d48d82dc3aed8
roksard <roksard@gmail.com>
2019-07-22 15:34:43 +0500

    190717-RESTnSpring-WS: add data classes

--- 190717-RESTnSpring-WS/src/main/java/ru/roksard/restnspringws/OrganisationController.java
+++ 190717-RESTnSpring-WS/src/main/java/ru/roksard/restnspringws/OrganisationController.java
@@ -1,20 +1,29 @@
 package ru.roksard.restnspringws;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController	
-public class GreetingController {
+@RequestMapping("/organisation")
+public class OrganisationController {
 	
 	
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 	
-	@RequestMapping("/greeting")
-	public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-		return new Greeting(counter.getAndIncrement(), String.format(template, name));
+	@GetMapping("/") //add paging params ?offset=0 &?limit=20
+	public List<Organisation> getOrganisationList() {
+		return null;
+	}
+	
+	@GetMapping("/{id}")
+	public Organisation getOrganisation(@PathVariable int id) {
+		return null;
 	}
 }
